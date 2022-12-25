b30da83f066943823a5f73ddf69b0a8bc54ebbfd
Mustafa Kamil <godric.phoenix@gmail.com>
2022-12-25 12:47:04 -0600

    understanding argparser

--- secX-lecX.pl
+++ secX-lecX.pl
@@ -1,17 +1,29 @@
 use strict;
 use warnings;
 use Data::Dumper;
-use Getopt::long;
+use Getopt::Long;
 
 $|=1;
 
 sub main{
-    my %opts = {
+    my %opts = (
         server => undef,
         userid => undef,
         verbose => undef,
-        s => undf,
-    }
+        s => undef,
+    );
+
+    GetOptions (\%opts,
+    "csi=s",
+    "server=s",
+    "userid=s",
+    "verbose",
+    "s=s");
+
+    print "$opts{server}\n";
+    print "$opts{userid}\n";
+    print "$opts{verbose}\n";
+    print "$opts{s}\n";
 }
 
 main();
